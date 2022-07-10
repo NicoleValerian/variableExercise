7880c90ef945e4ac798949f9509ce151ef0c59c6
NicoleValerian <nicolevalerian79@gmail.com>
2022-07-10 13:19:21 -0400

    adding file for variableExercise

---
+++ app.js
@@ -0,0 +1,6 @@
+const PI = 3.14;
+
+// 1. Var allows you to reassign and redeclare. Var gets saved to the global scope. Var gets hoisted to the beginning of the code. Let allows you to reassign but not redeclare. Let has a block scope. Let variables do not get hoiseted/run first. 
+// 2. Const does not allow you to reassign or redeclare. Const has a block scope. Const variables do not get hoisted/run first.
+// 3. Let allows you ro reassign. Const does now.
+// 4. Hoisting allows var variables to get lifted to the top of the code so they are identified first. This allows it to run as undefined if the variable is called before it is defined. 
